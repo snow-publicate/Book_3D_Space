909bc63fb1d161075074991505b938876fc18378
snow-webApp-004 <119712353+seoljoo-kang@users.noreply.github.com>
2023-01-16 15:39:33 +0900

    23.01.16-1539

--- Assets/Scripts/MoonMove.cs
+++ Assets/Scripts/MoonMove.cs
@@ -6,7 +6,7 @@ public class MoonMove : MonoBehaviour
     //다른 스크립트에서도 time변수에 접근할 수 있게 합니다.
     public GameObject target; //태양을 유니티 에디터에서 타겟으로 삼기 위한 변수 선언
     private Vector3 targetPosition; //태양의 위치를 담기 위한 변수 선언
-    private float earthTime; //지구의 속도를 담기 위한 변수 선언
+    private float earthSpeed; //지구의 속도를 담기 위한 변수 선언
 
     private void Start() 
     {
@@ -16,13 +16,13 @@ public class MoonMove : MonoBehaviour
     private void Update() 
     {
         //지구의 속도를 변수에 할당 및 달에 맞게 변환
-        earthTime = GameObject.Find("Earth").GetComponent<EarthMove>().speed; 
+        earthSpeed = GameObject.Find("Earth").GetComponent<EarthMove>().speed; 
 
         //본 스크립트가 부착된 객체를 반시계방향으로 자전 (달의 공전에 대응)
-        transform.Rotate(Vector3.down * Time.deltaTime * earthTime / 27f);
+        transform.Rotate(Vector3.down * Time.deltaTime * earthSpeed * 365f / 27f);
 
         //본 스크립트가 부착된 객체를 반시계방향으로 대상 중심으로 공전 
-        transform.RotateAround(targetPosition, Vector3.down, Time.deltaTime * earthTime);
+        transform.RotateAround(targetPosition, Vector3.down, Time.deltaTime * earthSpeed);
     }
 }
 
